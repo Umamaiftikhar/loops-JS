9466d1864ed6629d8e0d10740f268a9bdba92c4c
Umama Iftikhar Siddiqui <124132923+Umamaiftikhar@users.noreply.github.com>
2023-06-05 11:34:56 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,90 @@
+    <script>
+        var a,b;
+        for(a=1;a<=9;a++)
+        {
+            for(b=1;b<-a;b++)
+        {
+            document.write("*");
+        }
+        document.write("<br>");
+    }
+
+    {
+        for(a=1;a<=8;a++)
+        
+        {
+            document.write("*");
+        }
+        document.write("<br>");
+    }
+
+    {
+        for(a=1;a<=7;a++)
+    
+        {
+            document.write("*");
+        }
+        document.write("<br>");
+    }
+
+    {
+        for(a=1;a<=6;a++)
+    
+        {
+            document.write("*");
+        }
+        document.write("<br>");
+    }
+    {
+        for(a=1;a<=5;a++)
+    
+        {
+            document.write("*");
+        }
+        document.write("<br>");
+    }
+    {
+        for(a=1;a<=4;a++)
+    
+        {
+            document.write("*");
+        }
+        document.write("<br>");
+    }
+
+
+
+
+    {
+        for(a=1;a<=3;a++)
+    
+        {
+            document.write("*");
+        }
+        document.write("<br>");
+    }
+
+    {
+        for(a=1;a<=2;a++)
+    
+        {
+            document.write("*");
+        }
+        document.write("<br>");
+    }
+
+    {
+        for(a=1;a<=1;a++)
+    
+        {
+            document.write("*");
+        }
+        document.write("<br>");
+    }
+    </script>
+
+
+
+
+
+
